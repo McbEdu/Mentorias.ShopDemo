fe1c6a498b3fcf820b24f0b97cb3fc821dd03416
OtavioVB <otaviovb.developer@gmail.com>
2022-11-18 15:20:27 -0300

    feat: `Product Entity Abstraction`

---
+++ McbEdu.Mentorias.ShopDemo.Application/Models/Entities/ProductBase.cs
@@ -0,0 +1,29 @@
+﻿using McbEdu.Mentorias.ShopDemo.Domain.Models.ENUMs;
+
+namespace McbEdu.Mentorias.ShopDemo.Domain.Models.Entities;
+
+public abstract class ProductBase
+{
+    public Guid Identifier { get; }
+    public string Code { get; private set; }
+    public string Description { get; private set; }
+    public TypeProduct TypeProduct { get; }
+
+    protected ProductBase(Guid identifier, string code, string description, TypeProduct typeProduct)
+    {
+        Identifier = identifier;
+        Code = code;
+        Description = description;
+        TypeProduct = typeProduct;
+    }
+
+    public void ChangeCode(string code)
+    {
+        Code = code;
+    }
+
+    public void ChangeDescription(string description)
+    {
+        Description = description;
+    }
+}
