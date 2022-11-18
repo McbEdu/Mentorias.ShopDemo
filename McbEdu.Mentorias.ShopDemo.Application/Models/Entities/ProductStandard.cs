f2ff9a3777aff2fb342040f434fad33f8e4038fa
OtavioVB <otaviovb.developer@gmail.com>
2022-11-18 15:26:53 -0300

    feat: `Product Standard Implementation`

---
+++ McbEdu.Mentorias.ShopDemo.Application/Models/Entities/ProductStandard.cs
@@ -0,0 +1,10 @@
+﻿using McbEdu.Mentorias.ShopDemo.Domain.Models.ENUMs;
+
+namespace McbEdu.Mentorias.ShopDemo.Domain.Models.Entities;
+
+public class ProductStandard : ProductBase
+{
+    public ProductStandard(Guid identifier, string code, string description, TypeProduct typeProduct) : base(identifier, code, description, TypeProduct.Standard)
+    {
+    }
+}
