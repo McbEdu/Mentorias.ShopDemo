684b86c3c253b9cf21a7adaa88f08f1a3a8dc753
OtavioVB <otaviovb.developer@gmail.com>
2022-11-18 15:31:42 -0300

    base: `Create Product Handler`

---
+++ McbEdu.Mentorias.ShopDemo.Services/Handlers/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,16 @@
+﻿using McbEdu.Mentorias.ShopDemo.Domain.Contracts.Services.Handlers;
+
+namespace McbEdu.Mentorias.ShopDemo.Services.Handlers.CreateProduct;
+
+public class CreateProductHandler : HandlerBase<CreateProductResponse, CreateProductRequest>
+{
+    public CreateProductHandler()
+    {
+
+    }
+
+    public override Task<CreateProductResponse> Handle(CreateProductRequest request)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ McbEdu.Mentorias.ShopDemo.Services/Handlers/CreateProduct/Inputs/CreateProductInputModel.cs
@@ -0,0 +1,12 @@
+﻿namespace McbEdu.Mentorias.ShopDemo.Services.Handlers.CreateProduct.Inputs;
+
+public class CreateProductInputModel
+{
+    public string Code { get; set; }
+    public string Description { get; set; }
+
+    public CreateProductInputModel()
+    {
+        Code = Description = string.Empty;
+    }
+}
