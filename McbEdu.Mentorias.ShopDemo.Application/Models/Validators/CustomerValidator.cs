e1874ca59488009a07ab781900fe837f154627f9
OtavioVB <otaviovb.developer@gmail.com>
2022-11-18 16:04:03 -0300

    fix: `Customers Validators`

--- McbEdu.Mentorias.ShopDemo.Application/Models/Validators/CustomerValidator.cs
+++ McbEdu.Mentorias.ShopDemo.Application/Models/Validators/CustomerValidator.cs
@@ -9,44 +9,42 @@ public class CustomerValidator : AbstractValidator<CustomerBase>
     {
         RuleFor(p => p.Identifier.ToString()).Matches(@"^[a-zA-Z0-9]{8}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{12}$").WithMessage("O identificador precisa estar no padrão esperado.");
 
-        RuleFor(c => c.Name).NotEmpty().NotNull().WithMessage("O nome do cliente não pode ser nulo ou vazio.");
-        RuleFor(c => c.Name.Length).GreaterThan(2).WithMessage("O nome do cliente deve ter três caracteres ou mais.");
-        RuleFor(c => c.Name.Length).LessThan(51).WithMessage("O nome do cliente deve ter menos que 51 caracteres.");
+        RuleFor(c => c.Name).NotEmpty().NotNull().WithMessage("O nome do cliente não pode ser nulo ou vazio.").WithName(p => p.Name);
+        RuleFor(c => c.Name.Length).LessThan(51).WithMessage("O nome do cliente deve ter menos que 51 caracteres.").WithName(p => p.Name);
         RuleFor(c => c.Name).Custom((information, context) =>
         {
             foreach (var character in information)
             {
                 if (char.IsLetter(character) == false && information.ToLower().Contains("é") == false && information.ToLower().Contains("á") == false && char.IsWhiteSpace(character) == false)
                 {
-                    context.AddFailure($"O nome do cliente deve conter apenas caracteres adequados.");
+                    context.AddFailure(information.ToString(),$"O nome do cliente deve conter apenas caracteres adequados.");
                     return;
                 }
             }
         });
 
-        RuleFor(c => c.Surname).NotEmpty().NotNull().WithMessage("O sobrenome do cliente não pode ser nulo ou vazio.");
-        RuleFor(c => c.Surname.Length).GreaterThan(3).WithMessage("O sobrenome do cliente deve ter quatro caracteres ou mais.");
-        RuleFor(c => c.Surname.Length).LessThan(151).WithMessage("O sobrenome do cliente deve ter menos que 151 caracteres.");
+        RuleFor(c => c.Surname).NotEmpty().NotNull().WithMessage("O sobrenome do cliente não pode ser nulo ou vazio.").WithName(p => p.Surname);
+        RuleFor(c => c.Surname.Length).LessThan(151).WithMessage("O sobrenome do cliente deve ter menos que 151 caracteres.").WithName(p => p.Surname);
         RuleFor(c => c.Surname).Custom((information, context) =>
         {
             foreach (var character in information)
             {
                 if (char.IsLetter(character) == false && information.ToLower().Contains("é") == false && information.ToLower().Contains("á") == false && char.IsWhiteSpace(character) == false)
                 {
-                    context.AddFailure($"O sobrenome do cliente deve conter apenas caracteres adequados.");
+                    context.AddFailure(information, $"O sobrenome do cliente deve conter apenas caracteres adequados.");
                     return;
                 }
             }
         });
 
-        RuleFor(c => c.Email.Length).LessThan(151).WithMessage("O sobrenome do cliente deve ter menos que 151 caracteres.");
-        RuleFor(c => c.Email).EmailAddress().WithMessage("O email precisa estar em um formato válido!");
+        RuleFor(c => c.Email.Length).LessThan(151).WithMessage("O sobrenome do cliente deve ter menos que 151 caracteres.").WithName(p => p.Email);
+        RuleFor(c => c.Email).EmailAddress().WithMessage("O email precisa estar em um formato válido!").WithName(p => p.Email);
 
         RuleFor(c => c.Birthday).Custom((information, context) =>
         {
             if (information > DateTime.Now || information == DateTime.UnixEpoch)
             {
-                context.AddFailure($"A data de aniversário do cliente necessita ser válida!");
+                context.AddFailure(information.ToString(), $"A data de aniversário do cliente necessita ser válida!");
             }
         });
     }
