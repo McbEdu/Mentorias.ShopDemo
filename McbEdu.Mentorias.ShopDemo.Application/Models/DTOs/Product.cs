02e0c9e90510e333299727c5b65052be0cece563
OtavioVB <otaviovb.developer@gmail.com>
2022-11-18 15:11:08 -0300

    feat: `Data Transfer for Products`

---
+++ McbEdu.Mentorias.ShopDemo.Application/Models/DTOs/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace McbEdu.Mentorias.ShopDemo.Domain.Models.DTOs;
+
+public class Product
+{
+    public Guid Identifier { get; }
+    public string Code { get; }
+    public string Description { get; }
+
+    public Product()
+    {
+        Code = Description = string.Empty;
+    }
+}
