0c25a0e8bb74ea8f643a76132d237e89556271fd
OtavioVB <otaviovb.developer@gmail.com>
2022-11-18 15:33:58 -0300

    feat: `Data Context Product Db Set Property and Mapping`

--- McbEdu.Mentorias.ShopDemo.Infrascructure/Data/DataContext.cs
+++ McbEdu.Mentorias.ShopDemo.Infrascructure/Data/DataContext.cs
@@ -7,17 +7,21 @@ namespace McbEdu.Mentorias.ShopDemo.Infrascructure.Data;
 public class DataContext : DbContext
 {
     public DbSet<Customer> Customers { get; protected set; }
+    public DbSet<Product> Products { get; protected set; }
 
     private IBaseMapping<Customer> CustomerBaseMapping { get; }
+    private IBaseMapping<Product> ProductBaseMapping { get; }
 
-    public DataContext(DbContextOptions options, IBaseMapping<Customer> customerBaseMapping) : base(options)
+    public DataContext(DbContextOptions options, IBaseMapping<Customer> customerBaseMapping, IBaseMapping<Product> productBaseMapping) : base(options)
     {
         CustomerBaseMapping = customerBaseMapping;
+        ProductBaseMapping = productBaseMapping;
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         CustomerBaseMapping.CreateBaseMapping(modelBuilder.Entity<Customer>());
+        ProductBaseMapping.CreateBaseMapping(modelBuilder.Entity<Product>());
 
         base.OnModelCreating(modelBuilder);
     }
---
+++ McbEdu.Mentorias.ShopDemo.Infrascructure/Data/Mappings/ProductBaseMapping.cs
@@ -0,0 +1,13 @@
+﻿using McbEdu.Mentorias.ShopDemo.Domain.Contracts.Infrascructure.Mappings;
+using McbEdu.Mentorias.ShopDemo.Domain.Models.DTOs;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace McbEdu.Mentorias.ShopDemo.Infrascructure.Data.Mappings;
+
+public class ProductBaseMapping : IBaseMapping<Product>
+{
+    public void CreateBaseMapping(EntityTypeBuilder<Product> entity)
+    {
+        entity.HasKey(p => p.Identifier);
+    }
+}
