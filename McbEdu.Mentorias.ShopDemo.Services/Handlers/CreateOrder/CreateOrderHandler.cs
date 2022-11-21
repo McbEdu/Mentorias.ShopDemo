636dc2653c13b5114cc11e28395cb53a4851178b
OtavioVB <otaviovb.developer@gmail.com>
2022-11-21 19:19:12 -0300

    feat: `Handler Workflow Base`

---
+++ McbEdu.Mentorias.ShopDemo.Services/Handlers/CreateOrder/CreateOrderHandler.cs
@@ -0,0 +1,11 @@
+﻿using McbEdu.Mentorias.ShopDemo.Domain.Contracts.Services.Handlers;
+
+namespace McbEdu.Mentorias.ShopDemo.Services.Handlers.CreateOrder;
+
+public class CreateOrderHandler : HandlerBase<CreateOrderResponse, CreateOrderRequest>
+{
+    public override Task<CreateOrderResponse> Handle(CreateOrderRequest request)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ McbEdu.Mentorias.ShopDemo.Services/Handlers/CreateOrder/CreateOrderRequest.cs
@@ -0,0 +1,11 @@
+﻿using McbEdu.Mentorias.ShopDemo.Domain.Contracts.Services.Handlers;
+using McbEdu.Mentorias.ShopDemo.Domain.Models.ENUMs;
+
+namespace McbEdu.Mentorias.ShopDemo.Services.Handlers.CreateOrder;
+
+public class CreateOrderRequest : RequestBase
+{
+    public CreateOrderRequest(DateTime requestedOn, TypeVerbRequest typeVerbRequest) : base(requestedOn, typeVerbRequest)
+    {
+    }
+}
