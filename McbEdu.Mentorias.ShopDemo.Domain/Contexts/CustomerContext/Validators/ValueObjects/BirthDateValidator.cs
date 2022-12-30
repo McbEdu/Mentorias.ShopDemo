6a24b529c74336e66b83c761c48b490a7bc9b4a3
OtavioVB <otaviovb.developer@gmail.com>
2022-12-30 14:48:16 -0300

    fix: `DateTime Null Case Validation`

--- McbEdu.Mentorias.ShopDemo.Domain/Contexts/CustomerContext/Validators/ValueObjects/BirthDateValidator.cs
+++ McbEdu.Mentorias.ShopDemo.Domain/Contexts/CustomerContext/Validators/ValueObjects/BirthDateValidator.cs
@@ -8,5 +8,12 @@ public class BirthDateValidator : AbstractValidator<BirthDate>
     public BirthDateValidator()
     {
         RuleFor(p => p.GetValue()).LessThan(BirthDate.MinimumBirthdate).WithMessage("A data de nascimento precisa ser menor que o horário atual");
+        RuleFor(p => p.GetValue()).Custom((information, context) =>
+        {
+            if (information == DateTime.MinValue)
+            {
+                context.AddFailure("A data de nascimento não pode ser nula ou vazia.");
+            }
+        });
     }
 }
